bdc464403dbeb74fec59f967612d483df457aaec
Sylvie-B <unsolo@hotmail.fr>
2020-12-01 16:45:30 +0100

    creation des variables

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log(random());
+
+let nbrEnt = document.getElementById("entier");
+
+let nbrAVir = document.getElementById("flottant");
+
+let notANbr = document.getElementById("non-numerique");
+
+let alea = document.getElementById("random");
+
+let sansVir = document.getElementById("trunc");
+
+let newText = document.getElementById("replace");
+
+let operande = 10;
+
